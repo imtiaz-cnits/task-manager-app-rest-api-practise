02f3a9ab197895856125e1e7751a38efa8971e83
imtiaz-cnits <ia64744@gmail.com>
2024-03-21 04:57:55 +0600

    login complete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -12,17 +12,19 @@ exports.registration = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
+
   try {
     let reqBody = req.body;
     let user = await UsersModel.find(reqBody);
-
     if (user.length > 0) {
-
+      let Payload = {exp:Math.floor(Date.now()/1000) + (24*60*60), data:reqBody['email']};
+      let token = jwt.sign(Payload, '2664-anika');
+      res.json({status: "success", message: "User Found", token: token})
     }
     else {
       res.json({ status: "fail", message: "No user found" });
     }
-
+    res.json({ status: "success", message: user });
   } catch (err) {
     res.json({ status: "fail", message: err });
   }
@@ -37,6 +39,3 @@ exports.verifyEmail = (req, res) => {};
 exports.verifyOTP = (req, res) => {};
 
 exports.passwordReset = (req, res) => {};
-
-
-(())()
