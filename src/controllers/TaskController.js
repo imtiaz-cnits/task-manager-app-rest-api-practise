03e2c2c17e3c3045d10cbd990c1413e3675f7db8
imtiaz-cnits <ia64744@gmail.com>
2024-03-25 01:58:55 +0600

    Task REST API Completed

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,1 +1,53 @@
-const express = require("express");
+const TasksModel = require("../models/TasksModel");
+
+exports.create = async (req, res) => {
+    try {
+        let email = req.headers['email'];
+        let reqBody = req.body;
+        reqBody.email = email;
+        await TasksModel.create(reqBody);
+        res.json({status: "success", message: "Task Created Successfully"});
+    }
+    catch (err) {
+        res.json({status: "fail", message: err});
+    }
+}
+
+
+exports.update = async (req, res) => {
+    try {
+        let email = req.headers['email'];
+        let {id} = req.params;
+        let reqBody = req.body;
+        await TasksModel.updateOne({_id: id, email: email}, reqBody);
+        res.json({status: "success", message: "Task Updated"});
+    }
+    catch (err) {
+        res.json({status: "fail", message: err});
+    }
+}
+
+
+exports.read = async (req, res) => {
+    try {
+        let email = req.headers['email'];
+        let data = await TasksModel.find({email:email});
+        res.json({status: "success", data: data});
+    }
+    catch (err) {
+        res.json({status: "fail", message: err});
+    }
+}
+
+
+exports.delete = async (req, res) => {
+    try {
+        let email = req.headers['email'];
+        let {id} = req.params;
+        await TasksModel.deleteOne({_id: id, email: email});
+        res.json({status: "success", message: "Delete Completed"});
+    }
+    catch (err) {
+        res.json({status: "fail", message: err});
+    }
+}
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,6 @@
 const UsersModel = require("../models/UsersModel");
+const OTPModel = require("../models/OTPModel");
+const SendEmailUtility = require("../utility/SendEmailUtility");
 const jwt = require('jsonwebtoken');
 
 exports.registration = async (req, res) => {
@@ -18,7 +20,7 @@ exports.login = async (req, res) => {
     if (user.length > 0) {
       let Payload = {exp:Math.floor(Date.now()/1000) + (24*60*60), data:reqBody['email']};
       let token = jwt.sign(Payload, '123-xyz-abc');
-      res.json({status: "success", message: "User Found", token: token})
+      res.json({status: "success", message: "User Found", token: token});
     }
     else {
       res.json({ status: "fail", message: "No user found" });
@@ -51,9 +53,57 @@ exports.profileUpdate = async (req, res) => {
 };
 
 
+exports.verifyEmail = async (req, res) => {
+    try {
+        const {email} = req.params;
+        let user = await UsersModel.find({email: email});
+        if (user.length > 0) {
+            // Send Email
+            let otp = Math.floor(100000+Math.random()*900000);
+            await SendEmailUtility(email, `Your OTP is=${otp}`, "MERN 5 Task Manager Code");
+            await OTPModel.create({email: email, otp: otp, status: 'new'});
+            res.json({status: "success", message: "Verification Code has been sent to your email"});
+        } else {
+            res.json({ status: "Fail", message: "No User Found" });
+        }
+    } catch (err) {
+        res.json({status:"fail",message:err});
+    }
+};
 
-exports.verifyEmail = (req, res) => {};
 
-exports.verifyOTP = (req, res) => {};
 
-exports.passwordReset = (req, res) => {};
+exports.verifyOTP = async (req, res) => {
+    try {
+        const {email, otp} = req.params;
+        let user = await OTPModel.find({email: email, otp: otp, status: 'new'});
+        if (user.length > 0) {
+            await OTPModel.updateOne({email: email, otp: otp, status: 'Verified'});
+            res.json({status: "success", message: "Code Verification Success"});
+        } 
+        else {
+            res.json({ status: "Fail", message: "Invalid Code" });
+        }
+    } 
+    catch (err) {
+        res.json({status:"fail",message:err});
+    }
+};
+
+exports.passwordReset = async (req, res) => {
+    try {
+        const {email, otp, password} = req.params;
+        let user = await OTPModel.find({email: email, otp: otp, status: 'Verified'});
+        if (user.length > 0) {
+            await OTPModel.deleteOne({email: email, otp: otp});
+            await UsersModel.updateOne({email: email}, {password: password});
+            res.json({status: "success", message: "Password Update Success"});
+        } 
+        else {
+            res.json({ status: "Fail", message: "Invalid Request" });
+        }
+    } 
+    catch (err) {
+        res.json({status:"fail",message:err});
+    }
+};
